f43f035ab5291d0a351db6499356c5c0853811a8
Ersel <52753296+erselgemici@users.noreply.github.com>
2022-02-09 23:53:10 +0300

    While-ForEach

---
+++ Donguler-While-ForEach/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Donguler_While_ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1den başlayarak console dan girilen sayıya kadar (sayı dahil) ortalama hesaplayan program
+            Console.Write("Lütfen bir sayı giriniz:");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac ++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+            //a dan z ye kadar tüm harfleri console a yazdır.
+            char character = 'a';
+            while (character <= 'z')
+            {
+                Console.WriteLine(character);
+                character ++;
+            }
+
+            Console.WriteLine("*****ForEach*****");
+            string [] arabalar = {"Bmw","Ford","Toyota","Nissan"};
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+
+
+        }
+    }
+}
